6fbefae62285752003730705912da2aea1a056a0
SkylineVP <vladpolizp@gmail.com>
2020-01-14 18:59:01 +0200

    Slider Loader

--- src/components/SliderLoader/index.js
+++ src/components/SliderLoader/index.js
@@ -1,6 +1,6 @@
 import {loadJson} from "../../utils";
 import createSlider from './../Slider';
-
+const parentContainer = document.getElementById("testimonials");
 loadJson('./data/slides.json')
   .then(slides => {
     parentContainer.appendChild(createSlider(slides));
