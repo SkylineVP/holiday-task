39815bc11408f411156519be196ad13cfb56f52a
SkylineVP <vladpolizp@gmail.com>
2020-01-15 17:24:32 +0200

    Refactor structure project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
 import './assets/css/reset.css';
-import './assets/scss/layout.scss';
+import './assets/scss/addition/layout.scss';
 import './assets/scss/styles.scss';
 
-import './assets/js/OpenPictureLogic.js';
+import './assets/js/clickPictureInWorkLogic.js';
 import './assets/js/navigationLogic.js';
 import './components/EmployeesListLoader';
 import './components/SliderLoader';
